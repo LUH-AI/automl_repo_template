f5dc4259bc78d369372f9f9e978742669a6ca7b8
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2023-11-27 14:11:18 +0100

    this time add all files from branches

---
+++ luis/conda_setup/make_conda_env.sh
@@ -0,0 +1,3 @@
+# Create and activate env
+conda create -n $1 python=$2 -c conda-forge -y
+conda activate $1
---
+++ pc2/conda/gpu_example.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --job-name=test_gpu
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:a100:1
+#SBATCH --mem-per-cpu=10M
+#SBATCH --time=00:5:00
+#SBATCH --mail-user=<y.name>@ai.uni-hannover.de
+#SBATCH --mail-type=FAIL
+#SBATCH --output test_gpu-job_%j.out
+#SBATCH --error test_gpu-job_%j.err
+
+# Move to location of the runscript
+DIR=$(dirname $0)
+cd $REPODIR/$DIR
+
+# Activate the conda env
+conda-activate myenv
+
+python run_me.py --id $SLURM_ARRAY_TASK_ID
