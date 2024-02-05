a15c3354d25ad015ac0a2faba41cfc9984ccfccf
Theresa Eimer <t.eimer@ai.uni-hannover.de>
2024-02-05 15:31:13 +0100

    missing colors

--- cookiecutter.sh
+++ cookiecutter.sh
@@ -20,9 +20,9 @@ function yellow {
 
 # This should be in a pre_prompt.py hook, but atm cookiecutter has a bug related to those hooks
 # We should be able to move this after everyone can use a stable version (might be a while given some people work with python 3.9)
-echo "Do you want to use a ${BOLD}preset${NORMAL}?"
+echo -e "Do you want to use a ${BOLD}preset${NORMAL}?"
 echo "This will give you the best defaults (meaning you can skip everything after the command line interface choice)."
-echo "Options are: ${BOLD}${RED}'student'${NC}${NORMAL}, ${BOLD}${RED}'research'${NC}${NORMAL} and ${BOLD}${RED}'package'${NC}${NORMAL}:"
+echo -e "Options are: ${BOLD}${RED}'student'${NC}${NORMAL}, ${BOLD}${RED}'research'${NC}${NORMAL} and ${BOLD}${RED}'package'${NC}${NORMAL}:"
 read preset
 
 student="student"
@@ -39,26 +39,26 @@ else
     config_name="default.yaml"
 fi
 
-echo "Do you work with conda and want to directly ${BOLD}${RED}install${NC}${NORMAL} the repo? (y/n)"
+echo -e "Do you work with conda and want to directly ${BOLD}${RED}install${NC}${NORMAL} the repo? (y/n)"
 read conda
 
 yes="y"
 no="n"
 
 if [ "$conda" = "$yes" ] ; then
-    echo "Do you need a new conda env set up? (y/n)"
+    echo "Do you need a new conda env set up? (${BOLD}${RED}y${NC}${NORMAL}/${BOLD}${RED}n${NC}${NORMAL})"
     read conda_new
     if [ "$conda_new" = "$yes" ] ; then
-        echo $(yellow "Please enter your project name")
+        echo -e $(yellow "Please enter your project name")
         read name
-        echo $(yellow "Please enter your python version")
+        echo -e $(yellow "Please enter your python version")
         read version
 
         conda create -n $name python=$version -c conda-forge -y
         conda activate $name
         conda install gh --channel conda-forge
     else
-        echo $(yellow "Is your conda env activated? If no, please enter the name of your env.")
+        echo -e $(yellow "Is your conda env activated? If no, please enter the name of your env.")
         read env_name
         if [ "$env_name" = "$yes" ] ; then
             echo "Great, let's get started!"
